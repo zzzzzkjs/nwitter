f499a4980cb2f1cf30c57fdadc612427b49c7e6e
zzzzzkjs <zkjs@naver.com>
2020-10-22 21:29:57 +0900

    파이어베이스 설정

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import firebase from "../firebase.js";
+import AppRouter from "./Router.js";
+
+console.log(firebase);
+function App() {
+  return <AppRouter />;
+}
+
+export default App;
